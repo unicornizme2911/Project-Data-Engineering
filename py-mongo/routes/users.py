0f1cd08283caa45d123f2230a007200dfb4eda7a
2zz <kikinazzk98@gmail.com>
2023-06-15 18:58:02 +0700

    done

--- py-mongo/routes/users.py
+++ py-mongo/routes/users.py
@@ -2,13 +2,17 @@ from fastapi import APIRouter
 import bcrypt
 from models.users import Users
 from config.database import conn
-from schemas.users import userEntity, usersEntity
+from schemas.users import serializeDict, usersEntity
 from bson import ObjectId
 user = APIRouter()
 
 @user.get('/')
 async def find_all_users():
     return usersEntity(conn.local.users.find())
+
+@user.get('/{id}')
+async def find_one_user(id):
+    return serializeDict(conn.local.user.find_one({"_id":ObjectId(id)}))
 
 @user.post('/')
 async def create_user(user: Users):
@@ -17,9 +21,17 @@ async def create_user(user: Users):
     return usersEntity(conn.local.users.find())
 
 @user.put('/{id}')
-async def update_user(id,user:Users):
+async def update_user_by_id(id,user:Users):
     user.password = bcrypt.hashpw(user.password.encode(),bcrypt.gensalt())
     conn.local.users.find_one_and_update({"_id":ObjectId(id)},{
         "$set":dict(user)
     })
     return usersEntity(conn.local.users.find({"_id":ObjectId(id)}))
+
+@user.delete('/{id}')
+async def delete_user_by_id(id,user: Users):
+    return serializeDict(conn.local.user.find_one_and_delete({"_id":ObjectId(id)}))
+
+@user.delete('/{name}')
+async def delete_user_by_name(name,user: Users):
+    return serializeDict(conn.local.user.find_one_and_delete({"name":name}))
--- py-mongo/schemas/users.py
+++ py-mongo/schemas/users.py
@@ -1,11 +1,16 @@
-import bcrypt
 def userEntity(item) -> dict:
     return {
         "id":str(item["_id"]),
-        "name": str(item["name"]),
-        "email": str(item["email"]),
-        "password": str(item["password"])
+        "name":str(item["name"]),
+        "email":str(item["email"]),
+        "password":str(item["password"])
     }
 
 def usersEntity(entity) -> list:
     return [userEntity(item) for item in entity]
+
+def serializeDict(a) -> dict:
+    return {**{i:str(a[i]) for i in a if i=='_id'},**{i:a[i] for i in a if i!='_id'}}
+
+def serializeList(entity) -> list:
+    return [serializeDict(a) for a in entity]
